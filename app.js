f3822523a9bb4a523d6c61bb43acba5b1bb73b0b
Cecilia0292 <loureiro.ruthe@gmail.com>
2024-09-06 11:38:58 -0300

    atualização 06-09-projeto final

--- app.js
+++ app.js
@@ -4,27 +4,48 @@ function busca() {
   let section = document.getElementById("resultados-pesquisa");
   // Obtém a seção HTML com o ID "resultados-pesquisa" e armazena na variável 'section'.
   // Essa seção será utilizada para inserir os resultados da pesquisa.
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+  //se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Nada foi encontrado. Tente novamente!</p>"
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
 
   let resultados = "";
   // Inicializa uma string vazia para armazenar os resultados da pesquisa.
   // A cada iteração do loop, novos resultados serão concatenados a essa string.
 
   for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
     // Inicia um loop 'for...of' para iterar sobre cada elemento 'dado' do array 'dados'.
     // A cada iteração, um novo resultado será formatado e adicionado à string 'resultados'.
+    if (dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // cria um novo elemento
+      resultados += `
+    <div class="item-resultado">
+      <h2>
+        <a href="#"target="_blank">${dado.titulo}</a>
+      </h2>
+      <p class="descricao-meta">${dado.descricao}</p>
+      <a href=${dado.link} target="_blank">Buscar por município</a>
+    </div>
+  `;
+    }
 
-    resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#"target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href=${dado.link} target="_blank">Buscar por município</a>
-        </div>
-      `;
     // Dentro do loop, um novo elemento HTML <div> é criado para cada resultado.
     // As propriedades 'titulo', 'descricao' e 'link' de cada objeto 'dado' são utilizadas para preencher o conteúdo do HTML.
     // O resultado formatado é então concatenado à string 'resultados'.
+  }
+
+  if (!resultados) {
+    resultados = "<p>Nada foi encontrado. Tente novamente!</p>"
   }
 
   section.innerHTML = resultados;
--- dados.js
+++ dados.js
@@ -1,19 +1,22 @@
 let dados = [
     {
-        titulo: "ácido acetilsalicílico",
+        titulo: "ácido acetilsalicílico (AAS)",
         descricao: "O ácido acetilsalicílico (100mg e 500mg), faz parte do Componente Básico da Assistência Farmacêutica. Estes medicamentos tratam os principais problemas e condições de saúde da população brasileira. Para saber se seu município distribui de forma gratuita, clique no link abaixo.",
         link: "https://farmaciaestoque.manaus.am.gov.br/",
+        tags:"AAS Aas aas infantil",
     },
     {
         titulo: "cloridrato de bupropiona",
         descricao: "O cloridrato de bupropiona (150mg) comprimido de liberação prolongada, faz parte do Componente Estratégico da Assistência Farmacêutica (Cesaf). Para saber se seu município distribui de forma gratuita, clique no link abaixo.",
         link: "https://prefeitura.poa.br/sms/servicos/onde-esta-seu-medicamento",
+        tags:"Bup bup bupi ",
 
     },
     {
         titulo: "hemifumarato de quetiapina",
         descricao: "O hemifumarato de quetiapina (25 mg, 100mg, 200mg, 300mg), faz parte do Componente Especializado da Assistência Farmacêutica (Ceaf). Para saber se seu município distribui de forma gratuita, clique no link abaixo.",
         link: "https://e-saudesp.prefeitura.sp.gov.br/#/remedio-na-hora2",
+        tags:"queti Quetia",
     },
 
 ];
